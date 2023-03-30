862ecdbf66de4a08d0348b1b63f1df7b768f5d95
kautsarfrds <firdaus060402@gmail.com>
2023-03-30 23:41:23 +0700

    feat/update db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->string('photo', 2048)->nullable();
             $table->string('roles')->default('0');
             $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
             $table->timestamps();
 
         });
