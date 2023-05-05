4fea546074ed379b93c58e5a2faee9b1136b4911
fauzanafiansyah <fauzanafiansyah11@gmail.com>
2023-05-05 22:23:37 +0700

    feat/ wip history transaksi ref #33

--- resources/views/historytransaksi.blade.php
+++ resources/views/historytransaksi.blade.php
@@ -2,8 +2,30 @@
 
 @section('content')
 <div>
-    <div class="flex">
-        <h1 class="text-[#6A62C4] mt-[80px] ml-[185px] text-[16px]">History Transaksi</h1>
+    <h1 class="text-[#6A62C4] mt-[80px] ml-[185px] text-[16px]">History Transaksi</h1>
+    <div class="flex justify-center ">
+        <div class="w-[1220px] h-[1px] bg-[#6A62C4] mt-[8px] "></div>
     </div>
+
+    <div class="flex mt-[33px]">
+        <h1 class="ml-[257px]">Deskripsi</h1>
+        <h1 class="ml-[200px]">Waktu</h1>
+        <h1 class="ml-[400px]">Status Transaksi</h1>
+        <h1 class="ml-[200px]">Total</h1>
+    </div>
+
+    <div class="flex justify-center">
+        <div class="border w-[1190px] h-[660px] rounded-[12px] mt-[30px]">
+            <div class="flex justify-between">
+                <h1 class="">Deskripsi</h1>
+                <h1 class="">Waktu</h1>
+                <h1 class="">Status Transaksi</h1>
+                <h1 class="">Total</h1>
+            </div>
+        </div>
+    </div>
+
+
+
 </div>
 @endsection
