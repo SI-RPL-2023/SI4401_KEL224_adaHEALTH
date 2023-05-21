0dd85b37e9856e6592a3ff1967568051c628a2e7
auliaucheni <ulfaauliacheni@gmail.com>
2023-05-21 22:19:45 +0700

    feat/fix

--- resources/views/kategoriobat.blade.php
+++ resources/views/kategoriobat.blade.php
@@ -8,12 +8,12 @@
             <a href="" class="text-white">Obat & Vitamin</a>
         </div>
         <div class="flex flex-col justify-center items-center text-white">
-            <h1 class="font-bold text-3xl mt-[200px]">Kategori</h1>
+            <h1 class="font-bold text-3xl mt-[150px]">Kategori</h1>
             <div class="mt-[62px] flex gap-[46px] justify-center text-[#6A62C4]">
-                <div class="w-[251px] h-[155px] rounded-[20px] bg-white pb-[17px] flex justify-center items-end font-bold">FLU</div>
-                <div class="w-[251px] h-[155px] rounded-[20px] bg-white pb-[17px] flex justify-center items-end font-bold">SAKIT GIGI</div>
-                <div class="w-[251px] h-[155px] rounded-[20px] bg-white pb-[17px] flex justify-center items-end font-bold">LUKA KULIT</div>
-                <div class="w-[251px] h-[155px] rounded-[20px] bg-white pb-[17px] flex justify-center items-end font-bold">COVID-19</div>
+                <a href=""><div class="w-[251px] h-[155px] rounded-[20px] bg-white pb-[17px] flex justify-center items-end font-bold">FLU</div></a>
+                <a href=""><div class="w-[251px] h-[155px] rounded-[20px] bg-white pb-[17px] flex justify-center items-end font-bold">SAKIT GIGI</div></a>
+                <a href=""><div class="w-[251px] h-[155px] rounded-[20px] bg-white pb-[17px] flex justify-center items-end font-bold">LUKA KULIT</div></a>
+                <a href=""><div class="w-[251px] h-[155px] rounded-[20px] bg-white pb-[17px] flex justify-center items-end font-bold">COVID-19</div></a>
             </div>
             <h1 class="text-white text-[24px] mt-[70px] font-medium">Lainnya&nbsp;></h1>
         </div>
